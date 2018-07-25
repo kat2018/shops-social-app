a15aee07935339a8ff38d212798c72e5ad9158d0
kat2018 <kat.vge@gmail.com>
2018-07-25 18:35:25 -0400

    App.js Configuration

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const logger = require(morgan);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require(mongoose);
+mongoose.Promisse = global.Promise;
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/shops-social-app
 
-var app = express();
+const connection = mongoose.connection;
+connection.on("connected", () => {
+  console.log("Mongoose Connected Successfully");
+});
+//error catcher
+connection.on("error", err => {
+  console.log("Mongoose default connection error: " + err);
+});
 
-app.use(logger('dev'));
+const storesRouter = require("./routes/stores");
+const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
+
+const app = express();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", storesRouter);
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 module.exports = app;
