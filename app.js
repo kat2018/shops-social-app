5fa0c3c5f24098797e87683a452d9619dec37372
kat2018 <kat.vge@gmail.com>
2018-07-28 13:26:30 -0400

    setup posts route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ connection.on("error", err => {
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-// const postsRouter = require("./routes/posts");
+const postsRouter = require("./routes/posts/posts_controller");
 
 const app = express();
 
@@ -29,6 +29,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-// app.use("/posts", postsRouter);
+app.use("/posts", postsRouter);
 
 module.exports = app;
--- models/Post.js
+++ models/Post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const postSchema = require('../db/schemas/postSchema')
 
 const Post = mongoose.model('post', postSchema)
 
