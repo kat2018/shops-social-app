7ed9ad0d9df33a0c362fe1d1cebdf2b5b97d059b
kat2018 <kat.vge@gmail.com>
2018-07-29 17:55:46 -0400

    updated UserSchema

--- db/schemas/UserSchema.js
+++ db/schemas/UserSchema.js
@@ -1,45 +1,28 @@
 const Schema = require("mongoose").Schema;
 
-const UserSchema = new Schema({
-    userName: {},
-    email:{},
-    password:{},
+const UserSchema = new Schema(
+{
+    userName: {
+        type: String,
+        required: [true]
+    },
+    email:{
+        type: String
+    },
     age:{
         type: Number,
         min: [18, 'Must be 18 to post'],
         required: [true]
     },
-    phone: {
+    photoUrl: {
         type: String,
-        validate: {
-            validator: function (v) {
-                return /\d{3}-\d{3}-\d{4}/.test(v);
-            },
-            message: '{VALUE} is not a valid phone number!'
-        },
-        required: [true, 'User phone number required']
-    }
-})
-//custom validation from mongoose docs
-// let User = db.model('user', userSchema);
-// let user = new User();
-// let error;
-
-// user.phone = '555.0123';
-// error = user.validateSync();
-// assert.equal(error.errors['phone'].message,
-//     '555.0123 is not a valid phone number!');
-
-// user.phone = '';
-// error = user.validateSync();
-// assert.equal(error.errors['phone'].message,
-//     'User phone number required');
-
-// user.phone = '201-555-0123';
-// // Validation succeeds! Phone number is defined
-// // and fits `DDD-DDD-DDDD`
-// error = user.validateSync();
-// assert.equal(error, null);
-
+        default: 'https://cdn2.vectorstock.com/i/1000x1000/46/41/shopping-girl-vector-1224641.jpg'
+    },
+    stores: [ StoreSchema ]
+},
+{
+    timeStamps: {},
+}
+)
 
 module.exports = UserSchema
