7f3503d0ab8d9561152a531804952ca357be7b5e
kat2018 <kat.vge@gmail.com>
2018-07-28 20:57:20 -0400

    created css file for post container

---
+++ client/src/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                Hello From Post
+            </div>
+        );
+    }
+}
+
+export default Post;
--- client/src/PostContainer.js
+++ client/src/PostContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PostList from './PostList'
+import  './PostContainer.css'
 
 class PostContainer extends Component {
     render() {
         return 
-        <div>
+        <div className="box">
             <div className="reviews">
               <h2>Latest Reviews: </h2>
               <PostList />
