ab831091c8882ff3c339bcd29d04b359ef5295ae
kat2018 <kat.vge@gmail.com>
2018-07-29 19:33:11 -0400

    seeds and schema completed for User, Post and Store

--- db/seeds.js
+++ db/seeds.js
@@ -1,1 +1,36 @@
-require('dotenv').config
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+const { PostModel, StoreModel, UserModel} = require('./schema')
+
+//Users
+const Betty = new UserModel({
+    userName: 'betty white',
+    email: 'betty@w.io',
+    age: '96',
+    photoUrl: 'https://nyppagesix.files.wordpress.com/2018/07/betty-white.jpg?quality=90&strip=all',
+    stores: [lululemon, sephora]
+})
+
+//Stores
+const lululemon = new StoreModel({
+    storeName: 'lululemon',
+    storeDescription: 'Yoga gear',
+    userPosts: [PostSchema]
+})
+
+const sephora = new StoreModel({
+  storeName: "Sephora",
+  storeDescription: "Makeup and Skincare",
+  userPosts: [PostSchema]
+});
+
+//Posts
+const postSephora = new PostModel({
+    userComment: 'This is like a candy store! My niece loved it here!',
+    userSharedImageOfStore: 'https://aredlipandanudeshoe.files.wordpress.com/2015/03/sephora-selfie.jpg',
+    date: 02/02/2018
+})
+
+
+
