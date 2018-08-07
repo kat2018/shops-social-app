0bfa80c78306a0a6c658e1c142246be78c97f19a
kat2018 <kat.vge@gmail.com>
2018-08-07 19:54:03 -0400

    seeded successfully

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,4 @@
 class Store < ApplicationRecord
-      has_many :posts
-      has_many :users, through: :posts
+      has_many :posts, dependent: :destroy
+      has_many :users, through: :posts, dependent: :destroy
 end
