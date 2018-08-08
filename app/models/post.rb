13a638736710ddb2baf843adf51241eddaa4fc75
kat2018 <kat.vge@gmail.com>
2018-08-07 22:27:26 -0400

    attempt to push to heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,6 @@
 class Post < ApplicationRecord
       belongs_to :user
       belongs_to :store
-
-      validates :store, :presence => true
       
       accepts_nested_attributes_for :user, :store
 end
