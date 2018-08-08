f4a1d2114a3fa15dbf227381206d2e5a81a3f289
kat2018 <kat.vge@gmail.com>
2018-08-07 20:43:50 -0400

    test create on postman passed

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,14 +5,27 @@ class Api::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def create
+    @user = User.create(user_params)
+    render json: @user
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
   end
 
-  def delete
+  def destroy
+    @user = User.find(params[:id]).destroy
+    render status: :ok
+  end
+  private
+  def user_params
+    params.require(:user).permit(:username, :free_email, :image)
   end
 end
